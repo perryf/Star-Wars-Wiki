df1147d4e7a96fd7083fa4e522cca43a162b258d
Perry Fustero <pjfust@gmail.com>
2017-08-04 12:32:57 -0400

    working on seed data, trouble with migration now

--- db/migrate/20170804150133_create_characters.rb
+++ db/migrate/20170804150133_create_characters.rb
@@ -3,10 +3,14 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
     create_table :characters do |t|
       t.string :name, null: false
       t.string :species
-      t.string :class
+      t.string :class, default: ""
+      t.string :birth_year
+      t.string :height
+      t.string :mass
       t.text :bio, default: ""
       t.string :catch_phrase, default: ""
       t.string :img_url
+      t.references :alliance, index: true, foreign_key: true
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "swapi"
+
+Alliance.destroy_all
+Character.destroy_all
+
+characters_data = Swapi.get_all "people"
+
+characters = Characters.create([{
+  character_data.each do |character|
+    name: character["name"],
+    species: character.species,
+    birth_year: character.birth_year,
+    height: character.height,
+    mass: character.mass
+  end
+  },])
