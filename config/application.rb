a40af7b11e226bb785cfeeab546d5b85d6b7d629
Perry Fustero <pjfust@gmail.com>
2017-08-04 11:01:10 -0400

    start over, fix initial setup

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Starwars
+module StarWarsWiki
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
--- starwars/db/migrate/20170804145455_create_alliances.rb
+++
@@ -1,8 +0,0 @@
-class CreateAlliances < ActiveRecord::Migration[5.1]
-  def change
-    create_table :alliances do |t|
-
-      t.timestamps
-    end
-  end
-end
